2e2f015ffe406fa501b9ec1be822694b08ecbc86
etto48 <minecraft.etto@gmail.com>
2022-12-06 15:58:24 +0100

    fixed defines for linux

--- src/defines.hpp
+++ src/defines.hpp
@@ -6,14 +6,13 @@
 #define DEFAULT_PORT_STR "23232"
 #ifdef _WIN32
 #define MOKACCINO_ROOT (parsing::getenv("USERPROFILE")+"\\AppData\\Local\\Mokaccino")
+#define not !
+#define or ||
+#define and &&
 #else
 #define IsDebuggerPresent() false
 #define MOKACCINO_ROOT (parsing::getenv("HOME")+"/.config/Mokaccino")
 #endif
 #define CONFIG_PATH (MOKACCINO_ROOT+"/mokaccino.cfg")
-
-#define not !
-#define or ||
-#define and &&
 
 extern bool DEBUG;
